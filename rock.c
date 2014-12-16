9cc30db672cb7a7c3068862e27969c25a91c0e8c
Gareth <gareth.w.owen@gmail.com>
2014-12-16 21:27:28 +0000

    Finished animation code

--- rock.c
+++ rock.c
@@ -24,7 +24,7 @@ void addRock( int score )
 
   else if( score < 30000 )
   {
-    rockChance = rand % 30;
+    rockChance = rand() % 30;
   }
 
   else
@@ -44,10 +44,42 @@ void addRock( int score )
     return;
   }
 
-  
+  entity[i].x = SCREEN_WIDTH + rand() % 100;
+  entity[i].y = rand() % SCREEN_HEIGHT;
+  entity[i].action = &moveRock;
+  entity[i].speed = 1 + ( rand() % 5 );
+  entity[i].draw = &drawAnimatedEntity;
+  entity[i].animID = ROCK_ANIMATION;
+  entity[i].currentFrame = 0;
+  entity[i].frameTimer = 3;
+
+  SDL_QueryTexture( animation[entity[i].animID].frame[entity[i].currentFrame], NULL, NULL, &entity[i].w, &entity[i].h );
 }
 
 static void moveRock()
 {
+  /* Move the rock from right to left, then once it goes off the screen remove it */
+  self->x -= self->speed;
 
+  if( self->x <= -self->w )
+  {
+    self->active = 0;
+  }
+
+  /* update animation frame */
+
+  self->frameTimer--;
+
+  /* reset the frameTimer if it's come to the end of the frame and update frame */
+
+  if( self->frameTimer <= 0 )
+  {
+    self->frameTimer = 3;
+    self->currentFrame++;
+
+    if( self->currentFrame >= animation[self->animID].frameCount )
+    {
+      self->currentFrame = 0;
+    }
+  }
 }
