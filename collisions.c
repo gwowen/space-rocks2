b8d92fac9d8524358c89aac570c5434e73fcb0c5
Gareth <gareth.w.owen@gmail.com>
2014-12-17 17:03:56 +0000

    Added font drawing code and finished collisions

--- collisions.c
+++ collisions.c
@@ -2,3 +2,21 @@
 
 extern void addRandomParticle( float, float );
 extern void addScore();
+
+int collision( int, int, int, int, int, int, int, int );
+
+void doCollisions()
+{
+
+}
+
+int collision( int x0, int y0, int w0, int h0, int x2, int y2, int w1, int h1 )
+{
+  int x1 = x0 + w0;
+  int y1 = y0 + h0;
+
+  int x3 = x2 + w1;
+  int y3 = y2 + h1;
+
+  return !( x1 < x2 || x3 < x0 || y1 < y2 || y3 < y0 );
+}
